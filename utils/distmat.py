a38854be2d0ca5bfb6e7253b21d92e52fa162c35
Namgyu Ho <itsnamgyu@gmail.com>
2020-10-03 22:10:05 +0900

    Test and fix all util functions

--- utils/distmat.py
+++ utils/distmat.py
@@ -1,10 +1,12 @@
 """
-NOT TESTED
+Rerank code based on bag-of-tricks repo:
+https://github.com/michuanhaohao/reid-strong-baseline
 """
 import torch
+import numpy as np
 
 
-def compute_distmat(qf, gf):
+def compute_distmat(qf: torch.Tensor, gf: torch.Tensor):
     """
     Args:
         qf: Tensor(q, m)
@@ -14,29 +16,105 @@ def compute_distmat(qf, gf):
         Tensor(q, g)
         Euclidean squared distance
     """
-    assert(qf.shape[1] == gf.shape[1])
-    q = qf.shape[0]
-    g = gf.shape[1]
+    assert (qf.shape[1] == gf.shape[1])
+    q, g = qf.shape[0], gf.shape[0]
 
-    qq = qf.pow(2).sum(axis=1, keep_dims=True).expand(q, g)
-    gg = gf.pow(2).sum(axis=1, keep_dims=True).expand(g, q)
+    qq = qf.pow(2).sum(dim=1, keepdim=True).expand(q, g)
+    gg = gf.pow(2).sum(dim=1, keepdim=True).expand(g, q)
 
-    distmat = torch.addmm(beta=1, self=qq + gg.t(), alpha=-2, mat1=qf, mat2=gf.t())
+    distmat = (qq + gg.t()).addmm(mat1=qf, mat2=gf.t(), alpha=-2, beta=1)
 
     return distmat
 
 
-def compute_gallery_distmat(gf):
+def compute_inner_distmat(features: torch.Tensor):
     """
+    Used to obtain `gallery_distmat` or `all_distmat`
+
     Args:
-        gf: Tensor(g, m)
+        features: Tensor(x, m) (x = g for gallery_distmat, x = q + g for all_distmat)
 
     Returns:
-        Tensor(g, g)
+        Tensor(x, x)
         Euclidean squared distance
     """
-    g, m = gf.shape
-    gg = gf.pow(2).sum(axis=1, keep_dims=True).expand(g, g)
-    distmat = torch.addmm(beta=1, self=gg + gg.t(), alpha=-2, mat1=gf, mat2=gf.t())
+    n, m = features.shape
+    ff = features.pow(2).sum(dim=1, keepdim=True).expand(n, n)
+    distmat = (ff + ff.t()).addmm(mat1=features, mat2=features.t(), beta=1, alpha=-2)
 
     return distmat
+
+
+def rerank_distmat(all_distmat: torch.Tensor, q, k1=20, k2=6, lambda_value=0.3, cut=True):
+    """
+    Args:
+        all_distmat: Tensor(q + g, q + g)
+
+    Returns:
+        Tensor(q, g) if cut else Tensor(q + g, q + g)
+        Euclidean squared distance
+    """
+    all_distmat = all_distmat.numpy()  # TODO
+    assert(all_distmat.shape[0] == all_distmat.shape[1])
+    all_num = all_distmat.shape[0]
+    query_num = q
+    gallery_num = all_num  # simply matched to bot code. don't ask me.
+
+    original_dist = all_distmat
+    original_dist = np.transpose(original_dist / np.max(original_dist, axis=0))
+    V = np.zeros_like(original_dist).astype(np.float16)
+    initial_rank = np.argsort(original_dist).astype(np.int32)
+
+    print('starting re_ranking')
+    for i in range(all_num):
+        # k-reciprocal neighbors
+        forward_k_neigh_index = initial_rank[i, :k1 + 1]
+        backward_k_neigh_index = initial_rank[forward_k_neigh_index, :k1 + 1]
+        fi = np.where(backward_k_neigh_index == i)[0]
+        k_reciprocal_index = forward_k_neigh_index[fi]
+        k_reciprocal_expansion_index = k_reciprocal_index
+        for j in range(len(k_reciprocal_index)):
+            candidate = k_reciprocal_index[j]
+            candidate_forward_k_neigh_index = initial_rank[candidate, :int(np.around(k1 / 2)) + 1]
+            candidate_backward_k_neigh_index = initial_rank[candidate_forward_k_neigh_index,
+                                               :int(np.around(k1 / 2)) + 1]
+            fi_candidate = np.where(candidate_backward_k_neigh_index == candidate)[0]
+            candidate_k_reciprocal_index = candidate_forward_k_neigh_index[fi_candidate]
+            if len(np.intersect1d(candidate_k_reciprocal_index, k_reciprocal_index)) > 2 / 3 * len(
+                    candidate_k_reciprocal_index):
+                k_reciprocal_expansion_index = np.append(k_reciprocal_expansion_index, candidate_k_reciprocal_index)
+
+        k_reciprocal_expansion_index = np.unique(k_reciprocal_expansion_index)
+        weight = np.exp(-original_dist[i, k_reciprocal_expansion_index])
+        V[i, k_reciprocal_expansion_index] = weight / np.sum(weight)
+    original_dist = original_dist[:query_num, ]
+    if k2 != 1:
+        V_qe = np.zeros_like(V, dtype=np.float16)
+        for i in range(all_num):
+            V_qe[i, :] = np.mean(V[initial_rank[i, :k2], :], axis=0)
+        V = V_qe
+        del V_qe
+    del initial_rank
+    invIndex = []
+    for i in range(gallery_num):
+        invIndex.append(np.where(V[:, i] != 0)[0])
+
+    jaccard_dist = np.zeros_like(original_dist, dtype=np.float16)
+
+    for i in range(query_num):
+        temp_min = np.zeros(shape=[1, gallery_num], dtype=np.float16)
+        indNonZero = np.where(V[i, :] != 0)[0]
+        indImages = [invIndex[ind] for ind in indNonZero]
+        for j in range(len(indNonZero)):
+            temp_min[0, indImages[j]] = temp_min[0, indImages[j]] + np.minimum(V[i, indNonZero[j]],
+                                                                               V[indImages[j], indNonZero[j]])
+        jaccard_dist[i] = 1 - temp_min / (2 - temp_min)
+
+    final_dist = jaccard_dist * (1 - lambda_value) + original_dist * lambda_value
+    del original_dist
+    del V
+    del jaccard_dist
+    if cut:
+        final_dist = final_dist[:query_num, query_num:]
+    return final_dist
+
--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,18 +1,24 @@
 """
 Code based on bag-of-tricks repo:
 https://github.com/michuanhaohao/reid-strong-baseline
-
-NOT TESTED
 """
 import numpy as np
+from tqdm import tqdm
 
 
-def eval_func(distmat: np.ndarray, q_pids: np.ndarray, g_pids: np.ndarray, q_camids: np.ndarray, g_camids: np.ndarray,
-              max_rank=50):
+def evaluate(distmat: np.ndarray, q_pids: np.ndarray, g_pids: np.ndarray, q_camids: np.ndarray, g_camids: np.ndarray,
+              max_rank=50, test_ratio=1):
     """
     Evaluation with market1501 metric (used for Market, Duke, MSMT)
     Key: for each query identity, its gallery images from the same camera view are discarded.
+
+    Returns all_cnc, mAP, mINP
     """
+    if test_ratio > 1 or test_ratio < 0:
+        raise ValueError("test_ratio must by in [0, 1]")
+
+    q_pids, g_pids = np.array(q_pids, copy=False), np.array(g_pids, copy=False)
+    q_camids, g_camids = np.array(q_camids, copy=False), np.array(g_camids, copy=False)
     num_q, num_g = distmat.shape
     if num_g < max_rank:
         max_rank = num_g
@@ -23,8 +29,9 @@ def eval_func(distmat: np.ndarray, q_pids: np.ndarray, g_pids: np.ndarray, q_cam
     # compute cmc curve for each query
     all_cmc = []
     all_AP = []
+    all_INP = []
     num_valid_q = 0.  # number of valid query
-    for q_idx in range(num_q):
+    for q_idx in tqdm(range(int(num_q * test_ratio))):
         # get query pid and camid
         q_pid = q_pids[q_idx]
         q_camid = q_camids[q_idx]
@@ -38,9 +45,14 @@ def eval_func(distmat: np.ndarray, q_pids: np.ndarray, g_pids: np.ndarray, q_cam
         # binary vector, positions with value 1 are correct matches
         # E.g., 0 1 1 0 1 1 0 ...
         orig_cmc = matches[q_idx][keep]
+        #orig_cmc = matches[q_idx]
         if not np.any(orig_cmc):
             # this condition is true when query identity does not appear in gallery
             continue
+
+        final_rank = np.argmax(orig_cmc * np.arange(1, orig_cmc.shape[0] + 1)).astype(float) + 1
+        INP = orig_cmc.sum() / final_rank
+        all_INP.append(INP)
 
         # E.g., 0 1 x x x x ... to 0 0 1 1 1 1 ...
         cmc = orig_cmc.cumsum()
@@ -63,5 +75,6 @@ def eval_func(distmat: np.ndarray, q_pids: np.ndarray, g_pids: np.ndarray, q_cam
     all_cmc = np.asarray(all_cmc).astype(np.float32)
     all_cmc = all_cmc.sum(0) / num_valid_q
     mAP = np.mean(all_AP)
+    mINP = np.mean(all_INP)
 
-    return all_cmc, mAP
+    return all_cmc, mAP, mINP
